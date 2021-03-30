9371f246dc62ba6ba41040f8e7781c4480c63909
Ted John <ted@brambles.org>
2021-03-30 23:26:49 +0100

    Write new object identifiers to park file

--- src/openrct2/object/Object.cpp
+++ src/openrct2/object/Object.cpp
@@ -32,6 +32,36 @@ ObjectType& operator++(ObjectType& d, int)
     return d = (d == ObjectType::Count) ? ObjectType::Ride : static_cast<ObjectType>(static_cast<uint8_t>(d) + 1);
 }
 
+ObjectEntryDescriptor::ObjectEntryDescriptor(const rct_object_entry& newEntry)
+{
+    if (!object_entry_is_empty(&newEntry))
+    {
+        Generation = ObjectGeneration::DAT;
+        Entry = newEntry;
+    }
+}
+
+ObjectEntryDescriptor::ObjectEntryDescriptor(std::string_view newIdentifier)
+{
+    Generation = ObjectGeneration::JSON;
+    Identifier = std::string(newIdentifier);
+}
+
+bool ObjectEntryDescriptor::HasValue() const
+{
+    return Generation != ObjectGeneration::JSON || !Identifier.empty();
+};
+
+ObjectType ObjectEntryDescriptor::GetType() const
+{
+    return Generation == ObjectGeneration::JSON ? Type : Entry.GetType();
+}
+
+std::string_view ObjectEntryDescriptor::GetName() const
+{
+    return Generation == ObjectGeneration::JSON ? Identifier : Entry.GetName();
+}
+
 Object::Object(const rct_object_entry& entry)
 {
     _objectEntry = entry;
